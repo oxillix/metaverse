b3afb2d0368a3acbd5f8029357d797781f1e6e11
Nicolas Arnouts <nicolas.arnouts@outlook.com>
2023-09-30 22:26:51 +0200

    Hero, navbar, Explore, ExploreCard, customText

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -2,7 +2,7 @@
 
 import { motion } from "framer-motion";
 import styles from "../styles/index";
-import { slideIn, stagimport, staggerContainer, textVariant } from "../utils/motion";
+import { slideIn, staggerContainer, textVariant } from "../utils/motion";
 
 const Hero = () => (
   <section className={`${styles.yPaddings} sm:pl-16 pl-6`}>
@@ -25,15 +25,19 @@ const Hero = () => (
       </div>
 
       <motion.div variants={slideIn('right', 'tween', 0.2, 1)} className="relative w-full md:-mt-[20px] -mt-[12px]">
-        <div className="absolute w-full  h-[300px] hero-gradient rounded-top-[140px] z-[0] -top-[30px]">
+        <div className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-[0] -top-[30px]" />
           <img
             src="/cover.png"
             alt="cover"
             className="w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative"
           />
-        </div>
-      </motion.div>
 
+          <a href="#explore">
+            <div className="w-full flex justify-end sm:-mt-[70px] -mt-[50px] pr-[40px] relative z-10">
+              <img src="/stamp.png" alt="stamp" className="sm-w-[155px] w-[100px] sm:h-[155px] h-[100px] object-contain" />
+            </div>
+          </a>
+      </motion.div>
     </motion.div>
   </section>
 );
