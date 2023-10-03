2e52917b4ded85aebff42e7069fecdc04b5c77d2
Nicolas Arnouts <nicolas.arnouts@outlook.com>
2023-10-03 22:06:39 +0200

    Revert "[bugfix] normalized gradients z-axisses"
    This reverts commit 0dcfd609e4860cebf282aef9e8a3a40b2fcfbfab.

--- pages/index.js
+++ pages/index.js
@@ -27,18 +27,18 @@ const Page = () => (
     <Hero />
     <div className="relative">
       <About />
-      <div className="gradient-03 z-[-1]" />
+      <div className="gradient-03 z-0" />
       <Explore />
     </div>
     <div className="relative">
       <GetStarted />
-      <div className="gradient-04 z-[-1]" />
+      <div className="gradient-04 z-0" />
       <WhatsNew />
     </div>
     <World />
     <div className="relative">
       <Insights />
-      <div className="gradient-04 z-[-1]" />
+      <div className="gradient-04 z-0" />
       <Feedback />
     </div>
     <Footer />
